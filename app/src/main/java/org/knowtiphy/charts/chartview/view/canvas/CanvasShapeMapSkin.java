a526e4d62447f8061715446729a6ab8c71f7e588
graham@orangedogsoftware.com <graham@orangedogsoftware.com>
2025-08-03 12:15:58 +1200

    cleanup refactor shape map renderer to remove all view model stuff (so it is now a pure renderer of map layers)

---
+++ app/src/main/java/org/knowtiphy/charts/chartview/view/canvas/CanvasShapeMapSkin.java
@@ -0,0 +1,222 @@
+package org.knowtiphy.charts.chartview.view.canvas;
+
+import java.util.Collection;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import org.geotools.geometry.jts.JTS;
+import org.knowtiphy.charts.chartview.BaseMapViewModel;
+import org.knowtiphy.charts.chartview.view.ShapeMapBaseSkin;
+import org.knowtiphy.charts.chartview.view.ShapeMapView;
+import org.knowtiphy.charts.chartview.view.model.MapModel;
+import org.knowtiphy.shapemap.api.IMapLayer;
+import org.knowtiphy.shapemap.api.RenderingContext;
+import org.knowtiphy.shapemap.renderer.RendererUtilities;
+import org.knowtiphy.shapemap.renderer.ShapeMapRenderer;
+import org.knowtiphy.shapemap.renderer.Transformation;
+
+/**
+ * A skin for a shape map view that uses a JavaFX canvas to show an ESRI shape map of layers of
+ * features of some schema type.
+ *
+ * @param <S> the type of the schema
+ * @param <F> the type of the features
+ */
+public class CanvasShapeMapSkin<S, F> extends ShapeMapBaseSkin<S, F> {
+
+    private static final double PREFERRED_WIDTH = Region.USE_COMPUTED_SIZE;
+
+    private static final double PREFERRED_HEIGHT = Region.USE_COMPUTED_SIZE;
+
+    private final BaseMapViewModel<S, F> viewModel;
+
+    private final Color background;
+
+    private final StackPane root = new StackPane();
+
+    public CanvasShapeMapSkin(
+            ShapeMapView<S, F> surface, BaseMapViewModel<S, F> viewModel, Color background) {
+        super(surface);
+
+        this.viewModel = viewModel;
+        this.background = background;
+
+        for (MapModel<S, F> map : viewModel.maps()) {
+            root.getChildren().add(new BorderPane());
+        }
+
+        getChildren().addAll(root);
+        initGraphics();
+    }
+
+    private void initGraphics() {
+        if (Double.compare(S().getPrefWidth(), 0.0) <= 0
+                || Double.compare(S().getPrefHeight(), 0.0) <= 0
+                || Double.compare(S().getWidth(), 0.0) <= 0
+                || Double.compare(S().getHeight(), 0.0) <= 0) {
+            if (S().getPrefWidth() > 0 && S().getPrefHeight() > 0) {
+                S().setPrefSize(S().getPrefWidth(), S().getPrefHeight());
+            } else {
+                S().setPrefSize(PREFERRED_WIDTH, PREFERRED_HEIGHT);
+            }
+        }
+    }
+
+    @Override
+    public void layoutChildren(
+            final double x, final double y, final double width, final double height) {
+        System.err.println("layout children");
+        super.layoutChildren(x, y, width, height);
+        repaint();
+    }
+
+    private void repaint() {
+        var width = (int) root.getWidth();
+        var height = (int) root.getHeight();
+
+        if (viewModel.maps().isEmpty()) {
+            var canvas = new Canvas(width, height);
+            var graphics = canvas.getGraphicsContext2D();
+            graphics.setFill(background);
+            graphics.fillRect(0, 0, width, height);
+            root.getChildren().add(new BorderPane(canvas));
+            return;
+        }
+
+        //    var removeAdd = viewModel.maps().size() - root.getChildren().size();
+        //    System.err.println("Pane calculation ");
+        //    System.err.println("\tnum maps " + viewModel.maps().size());
+        //    System.err.println("\tnum panes " + root.getChildren().size());
+        //    System.err.println("\tremoveAdd " + removeAdd);
+        //    if(removeAdd > 0)
+        //    {
+        //      System.err.println("Adding panes " + removeAdd);
+        //      for(var i = 0; i < removeAdd; i++)
+        //      {
+        //        root.getChildren().add(new BorderPane());
+        //      }
+        //    }
+        //    else if(removeAdd < 0)
+        //    {
+        //      System.err.println("Removing panes " + removeAdd);
+        //      root.getChildren().remove(root.getChildren().size() + removeAdd,
+        // root.getChildren().size());
+        //    }
+        root.getChildren().clear();
+        int which = 0;
+        //    assert root.getChildren().size() == viewModel.maps().size();
+        try {
+            var foo =
+                    RendererUtilities.worldToScreenTransform(
+                            viewModel.bounds(),
+                            new Rectangle2D(0, 0, width, height),
+                            viewModel.crs());
+            var overallWts = new Transformation(foo);
+            overallWts.apply(viewModel.bounds().getMinX(), viewModel.bounds().getMaxY());
+            System.err.println(overallWts.getX() + " , " + overallWts.getY());
+
+            if (true) {
+                var canvas = new Canvas(width, height);
+                var graphics = canvas.getGraphicsContext2D();
+                graphics.setFill(background);
+                graphics.fillRect(0, 0, width, height);
+                var b = new BorderPane();
+                b.setCenter(canvas);
+                root.getChildren().add(b);
+
+                for (var model : viewModel.maps()) {
+                    System.err.println("which = " + which);
+                    try {
+                        var rendererContext =
+                                new RenderingContext<>(
+                                        layers(model),
+                                        model.totalRuleCount(),
+                                        JTS.toEnvelope(model.geometry()),
+                                        //  TODO -- shouldn't this be in the viewport?
+                                        new Rectangle2D(0, 0, width, height),
+                                        foo,
+                                        foo.createInverse(),
+                                        //        wts,
+                                        //      wts.createInverse(),
+                                        viewModel.adjustedDisplayScale(),
+                                        viewModel.featureAdapter(),
+                                        viewModel.renderablePolygonProvider(),
+                                        viewModel.svgProvider(),
+                                        viewModel.textSizeProvider());
+
+                        var renderer = new ShapeMapRenderer<>(rendererContext, graphics);
+                        renderer.paint();
+                        //      ((BorderPane) root.getChildren().get(which)).setCenter(canvas);
+                        which++;
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            } else {
+                for (var model : viewModel.maps()) {
+                    System.err.println("which = " + which);
+                    //        if(which != 1)
+                    //        {
+                    //          which++;
+                    //          continue;
+                    //        }
+
+                    //          try
+                    //          {
+                    //          var envelope = model.geometry().getEnvelopeInternal();
+                    //          var screenArea = screenArea(overallWts, envelope);
+                    //          var wts = RendererUtilities.worldToScreenTransform(envelope,
+                    // screenArea, viewModel
+                    //          .crs());
+                    var canvas = new Canvas(width, height);
+                    var graphics = canvas.getGraphicsContext2D();
+                    graphics.setFill(which == 0 ? background : Color.TRANSPARENT);
+                    graphics.fillRect(0, 0, width, height);
+                    var b = new BorderPane();
+                    root.getChildren().add(b);
+
+                    var rendererContext =
+                            new RenderingContext<>(
+                                    layers(model),
+                                    model.totalRuleCount(),
+                                    JTS.toEnvelope(model.geometry()),
+                                    //  TODO -- shouldn't this be in the viewport?
+                                    new Rectangle2D(0, 0, width, height),
+                                    foo,
+                                    foo.createInverse(),
+                                    //        wts,
+                                    //      wts.createInverse(),
+                                    viewModel.adjustedDisplayScale(),
+                                    viewModel.featureAdapter(),
+                                    viewModel.renderablePolygonProvider(),
+                                    viewModel.svgProvider(),
+                                    viewModel.textSizeProvider());
+
+                    var renderer = new ShapeMapRenderer<>(rendererContext, graphics);
+                    renderer.paint();
+                    b.setCenter(canvas);
+                    //      ((BorderPane) root.getChildren().get(which)).setCenter(canvas);
+                    which++;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //  TODO this is clumsy
+    private Collection<IMapLayer<S, F>> layers(MapModel<S, F> mapModel) {
+        return mapModel.layers().stream().map(l -> (IMapLayer<S, F>) l).toList();
+    }
+    //    private Rectangle2D screenArea(Transformation tx, Envelope envelope)
+    //    {
+    //        tx.apply(envelope.getMinX(), envelope.getMaxY());
+    //        var minX = tx.getX();
+    //        var minY = tx.getY();
+    //        tx.apply(envelope.getMaxX(), envelope.getMinY());
+    //        return new Rectangle2D(minX, minY, tx.getX() - minX, tx.getY() - minY);
+    //    }
+}
